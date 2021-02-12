8f5382fe3906aac86fc720b814e4e438e7abd01d
technomanya <38087296+technomanya@users.noreply.github.com>
2021-02-12 16:18:04 +0300

    yeni kapınız hayırlı olsun

--- NeonRace3D/Assets/Door.cs
+++ NeonRace3D/Assets/Door.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] private Transform doorPivot;
+    [SerializeField] private bool doorFall = false;
+    [SerializeField] private float speed = 1; 
+
+
+    void Update()
+    {
+        if (doorFall)
+        {
+            doorPivot.localRotation = Quaternion.Slerp(doorPivot.localRotation, Quaternion.Euler(-90, -180, 320), Time.deltaTime * speed);
+
+        }
+    }
     void OnCollisionEnter(Collision col)
     {
         if (col.transform.CompareTag("Player"))
         {
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().isKinematic = false;
+            doorFall = true;
+            //GetComponent<Rigidbody>().useGravity = true;
+            //GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
